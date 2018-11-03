97f688b5872f63a13dc6ba704ca7ce65ac9af65d
Adam Victor <avictor@berkeley.edu>
2018-11-03 13:13:20 -0700

    Updates

---
+++ precache-manifest.2c5e9ced70a453b7b913d1bb32feeacb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "865d70b5ca7ed1c722b8",
+    "url": "/gather-your-party/static/js/runtime~main.865d70b5.js"
+  },
+  {
+    "revision": "3cb157aee92818f3a298",
+    "url": "/gather-your-party/static/js/main.3cb157ae.chunk.js"
+  },
+  {
+    "revision": "3116373b773475c65b60",
+    "url": "/gather-your-party/static/js/1.3116373b.chunk.js"
+  },
+  {
+    "revision": "3cb157aee92818f3a298",
+    "url": "/gather-your-party/static/css/main.a94ec2dc.chunk.css"
+  },
+  {
+    "revision": "202bb9d0de5d8fa7be4926748c664fef",
+    "url": "/gather-your-party/index.html"
+  }
+];
